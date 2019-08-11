59af382887dc98f1053f28a7debb2caef6b1cd90
Chandon Porter <cneilporter94@gmail.com>
2019-08-11 17:15:27 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -2,5 +2,13 @@ class Meal
 
 attr_accessor :waiter, :customer, :total, :tip
 
-  def initialize()
+@@all = []
+
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+  end
+  
 end
