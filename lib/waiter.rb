72e5d77df15875fe2425c149215581c87b1e8786
Chandon Porter <cneilporter94@gmail.com>
2019-08-11 17:28:51 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -14,8 +14,8 @@ attr_accessor :name, :yrs_experience
     @@all
   end
   
-  def new_meal(waiter, total, tip=0)
-    Meal.new(waiter, self, total, tip)
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
   end
   
 end
