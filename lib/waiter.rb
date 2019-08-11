5c8c5b26abd7b942ed58a58e0bae635f20d721d9
Chandon Porter <cneilporter94@gmail.com>
2019-08-11 17:11:29 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,17 @@
 class Waiter
-
+  
+attr_accessor :name, :year 
+  
+  @@all = []
+  
+  def initialize(name, year)
+    @name = name
+    @year = year 
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
